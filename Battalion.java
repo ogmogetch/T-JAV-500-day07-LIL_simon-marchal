d24c85e121529b130b3a4c873f4879e597477ded
simon marchal <simon.marchal@epitech.eu>
2023-10-31 06:07:07 -0400

    update

--- Battalion.java
+++ Battalion.java
@@ -18,6 +18,29 @@ public class Battalion {
         }
     }
 
+    public boolean fight() {
+        if (characters.size() < 2) {
+            return false; // No characters to fight
+        }
+
+        Character first = characters.get(0);
+        Character second = characters.get(1);
+
+        int comparison = first.compareTo(second);
+        if (comparison > 0) {
+            characters.remove(1); // Remove second character (loser)
+        } else if (comparison < 0) {
+            characters.remove(0); // Remove first character (loser)
+        } else {
+            characters.remove(0); // Remove first character in case of a tie
+            characters.remove(0); // Remove second character in case of a tie
+        }
+
+        return true; // Indicates a fight occurred
+    }
+
+
+
     public static void main(String[] args) {
         List<Mage> mages = new ArrayList<>();
         mages.add(new Mage("Merlin"));
